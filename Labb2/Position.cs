a42b3f21ef18ca17f7f64ee292f974a697e27ad3
Hanna Östling <hannaoliviaostling@gmail.com>
2018-11-17 17:28:43 +0100

    G done

--- Labb2/Position.cs
+++ Labb2/Position.cs
@@ -1,10 +1,74 @@
 ﻿using System;
-namespace Labb2
-{
-    public class Position
-    {
-        public Position()
-        {
+
+namespace Labb2 {
+
+    public class Position {
+
+        private int xPosition;
+        private int yPosition;
+
+        public int XPosition { get { return xPosition; } set { xPosition = value < 0 ? 0 : value; } }
+        public int YPosition { get { return yPosition; } set { yPosition = value < 0 ? 0 : value; } }
+
+        public Position(int xPosition, int yPosition) {
+            XPosition = xPosition;
+            YPosition = yPosition;
         }
+
+        // Returnerar en double med avståndet från origo.
+        public double Length() {
+            double length = Math.Sqrt(Math.Pow(XPosition, 2) + Math.Pow(YPosition, 2));
+            return length;
+        }
+
+        // Returnerar sant om inskrivna positionen är lika med X och Y.
+        public bool Equals(Position p) {
+            return (p.XPosition == XPosition && p.YPosition == YPosition);
+        }
+
+        // Returnerar en kopia av inskrivna kordinaten.
+        public Position Clone() {
+            return new Position(XPosition, YPosition);
+        }
+
+        // Returnerar en sträng i formatet: (X,Y)
+        public override string ToString() {
+            return "(" + XPosition + "," + YPosition + ")";
+        }
+
+        // Returnerar sant om första positionen ligger närmast origo.
+        public static bool operator > (Position p1, Position p2) {
+            if (p1.Length() > p2.Length()) {
+                return true;
+            }
+            return false;
+        }
+
+        //  Returnerar sant om andra positionen ligger närmast origo.
+        public static bool operator < (Position p1, Position p2) {
+            if (p1.Length() < p2.Length()) {
+                return true;
+            }
+            return false;
+        }
+
+        // Returnerar en ny kordinat, den första positionen plus den andra positionen.
+        public static Position operator + (Position p1, Position p2) {
+            return new Position(p1.XPosition + p2.XPosition, p1.YPosition + p2.YPosition);
+        }
+
+        // Om position 1 är mindre än position 2, returnera x2-x1 och y2-y1, annars returnera x1-x2 och y1-y2.
+        public static Position operator - (Position p1, Position p2) {
+            if (p1 < p2) {
+                return new Position(p2.XPosition - p1.XPosition, p2.YPosition - p1.YPosition);
+            }
+            return new Position(p1.XPosition - p2.XPosition, p1.YPosition - p2.YPosition);
+        }
+
+        // Räknar ut avståndet mellan två positioner.
+        public static double operator % (Position p1, Position p2) {
+            return Math.Sqrt(Math.Pow(p1.XPosition - p2.XPosition, 2) + Math.Pow(p1.YPosition - p2.YPosition, 2));
+        }
+
     }
 }
--- Labb2/SortedPosList.cs
+++ Labb2/SortedPosList.cs
@@ -1,10 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Labb2
 {
     public class SortedPosList
     {
-        public SortedPosList()
+        // Lista "Positions" privat eftersom den endast ska användas i den här klassen.
+        private List<Position> Positions { get; set; }
+
+        // Sorterad lista efter positioner.
+        public SortedPosList() { Positions = new List<Position>(); }
+
+        // Returnerar listan som sträng med ett kommatecken mellan kordinaterna.
+        public override string ToString() { return string.Join(",", Positions); }
+
+        // Returnerar antalet positioner som finns i listan.
+        public int Count() { return Positions.Count; }
+
+        // Lägger till en position till listan, och hamnar på rätt ställe i listan (sorteras på avstånd till origo).
+        public void Add(Position p)
         {
+            for (int i = 0; i < Count(); i++)
+            {
+                if (Positions[i].Length() > p.Length())
+                {
+                    Positions.Insert(i, p);
+                    return;
+                }
+            }
+            Positions.Insert(Count(), p);
         }
+
+        // Tar bort en positoin från listan. Returnerar sant om positionen fanns i listan och togs bort.
+        public bool Remove(Position p)
+        {
+            for (int i = 0; i < Count(); i++)
+            {
+                if (p.XPosition == Positions[i].XPosition && p.YPosition == Positions[i].YPosition)
+                {
+                    Positions.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Returnerar en ny instans av SortedPosList, som innehåller kopior av alla punkter som finns i listan. Gör en deep clone.
+        public SortedPosList Clone()
+        {
+            SortedPosList posList = new SortedPosList();
+            foreach (Position position in Positions)
+            {
+                posList.Add(position.Clone());
+            }
+            return posList;
+        }
+
+        // Returnerar en ny lista som innehåller alla positioner som befinner sig inom en given cirkel.
+        public SortedPosList CircleContent(Position centerPosition, double radius)
+        {
+            SortedPosList circleContList = new SortedPosList();
+            foreach (Position position in Positions)
+            {
+                if (Math.Pow((position.XPosition - centerPosition.XPosition), 2) + Math.Pow((position.YPosition - centerPosition.YPosition), 2) < Math.Pow(radius, 2))
+                {
+                    circleContList.Add(position.Clone());
+                }
+            }
+            return circleContList;
+        }
+
+        // Returnerar en ny lista som är två listor ihopslagna till en lista.
+        public static SortedPosList operator +(SortedPosList list1, SortedPosList list2)
+        {
+            SortedPosList sortedPosList = new SortedPosList();
+            foreach (Position position in list1.Positions)
+            {
+                sortedPosList.Add(position.Clone());
+            }
+            foreach (Position position in list2.Positions)
+            {
+                sortedPosList.Add(position.Clone());
+            }
+            return sortedPosList;
+        }
+
+        // Hämta ett element på en given position i listan.
+        public Position this[int index] => Positions[index];
+
     }
 }
